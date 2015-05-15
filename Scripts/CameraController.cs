130767648fdf31190c36d7ddcc33298f27c247dc
Gal Koren <galkrn@gmail.com>
2015-05-15 15:54:00 -0500

    added comments and clean up scripts

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿// This script handles the change of projection relative to the viewer perspective.
+// It should be a component of the main camera.
+
+using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour 
@@ -8,14 +11,13 @@ public class CameraController : MonoBehaviour
 	public float zFactor = 10.0f;
 	public float xFactor = -10.0f;
 	public float distanceFromKinectInMeters = 2.0f;
-	public bool flipScreen = false;
 	public float cameraHeight = 7.0f;
+	public float cameraHeightFactor = 0.8f;
+	public GameObject projectionScreen;
+	public bool estimateViewFrustum = true;
+	public Transform[] Corners;
+	public Transform lookTarget;
 
-
-
-
-
-	
 	void Start()
 	{
 	}
@@ -25,7 +27,6 @@ public class CameraController : MonoBehaviour
 	private Vector3 getPositionsMean(ArrayList positions, int N) {
 		Vector3 sum = new Vector3(0.0f, 0.0f, 0.0f);
 		int toSumQuantity = Mathf.Min(N, positions.Count);
-		
 		if (toSumQuantity == 0)
 			return sum;
 		
@@ -56,53 +57,29 @@ public class CameraController : MonoBehaviour
 				{
 					Vector3 posJoint = manager.GetRawSkeletonJointPos(userId, iJointIndex);
 
-					Debug.Log(posJoint);
+					// Ignore positions too close to the Kinect
 					if(posJoint.z < 0.5f) return;
 
+					// Smooth signal by using averaging
 					Vector3 meanLastPosition = getPositionsMean(lastHeadPositions, 4);
 
 					if(lastHeadPositions.Count > 8 && Vector3.Distance(meanLastPosition, posJoint) > 0.8f) {
-						// outlier
-						//return;
+						// Handle outlier
 					}
 
+					// Save the most recent head coordinates
 					lastHeadPositions.Add (posJoint);
 					if (lastHeadPositions.Count >= 10) {
 						lastHeadPositions.RemoveAt(0);
 					}
 
-					//Vector3 camPos = getPositionsMean(lastHeadPositions, 3);
 					Vector3 camPos = posJoint;
-
-					//Debug.Log(camPos.z * 10.0f);
 
 					camPos.z = (camPos.z*10.0f - (17.5f * (2.0f/distanceFromKinectInMeters))) * zFactor;
 
-					//posJoint.Normalize();
+					Camera.main.transform.position = new Vector3 (camPos.x * xFactor * (-1.0f) * cameraHeightFactor, cameraHeight, camPos.z * (-1.0f) * cameraHeightFactor);
 
-					//posJoint.x *= xFactor;
-					//posJoint.z *= zFactor;
-					//camPos.x *= -1.0f;
-					//posJoint.z -= 0.9f;
-
-					float cameraHeightFactor = 0.8f;
-					//cameraHeightFactor = 1.0f;
-
-					//Camera.main.transform.position = new Vector3 (camPos.x * xFactor * (-1.0f) * cameraHeightFactor, cameraHeight, camPos.z);
-
-					if(flipScreen) {
-						Camera.main.transform.position = new Vector3 (camPos.z * (-1.0f) * cameraHeightFactor, cameraHeight, camPos.x * xFactor * cameraHeightFactor);
-					} else {
-						Camera.main.transform.position = new Vector3 (camPos.x * xFactor * (-1.0f) * cameraHeightFactor, cameraHeight, camPos.z * (-1.0f) * cameraHeightFactor);
-					}
-
-					doThis ();
-/*
-					GameObject[] allObjects = GameObject.FindGameObjectsWithTag("Untagged") ;
-					foreach(GameObject go in allObjects){
-							float distance = Vector3.Distance (Camera.main.transform.position, go.transform.position);
-							go.transform.localScale *= Camera.main.transform.position.y/distance;
-					}*/
+					changePerspective ();
 				}
 				
 			}
@@ -110,24 +87,10 @@ public class CameraController : MonoBehaviour
 		}
 	}
 
-
-
-
-	public GameObject projectionScreen;
-	public bool  estimateViewFrustum = true;
-
-	
-
-
-
-
-
-
-
-
-
-
-	void  doThis (){
+	// Code for the perspective projection transformation was extracted from
+	// http://en.wikibooks.org/wiki/Cg_Programming/Unity/Projection_for_Virtual_Reality
+	// and altered to fit this environment
+	void  changePerspective (){
 		if (null != projectionScreen)
 		{
 
@@ -274,111 +237,5 @@ public class CameraController : MonoBehaviour
 			}
 		}
 	}
-
-
-
-
-
-
-
-
-	
-	
-	public Transform[] Corners;
-	public Transform lookTarget;
-	/*
-	public bool drawNearCone, drawFrustum;
-	public float near = 10.0f;
-	
-	//Camera theCam;
-	/*
-	void Start () {
-		theCam = camera;Camera cam = Camera.main;
-	}
-	void Update() {
-		//Camera.main.transform.position = new Vector3 (Camera.main.transform.position.x, 6.0f, 0.0f);
-		UpdateMainCamera (Camera.main.transform.position);
-	}
-	
-	void UpdateMainCamera (Vector3 pe) {
-		Vector3 pa, pb, pc, pd;
-		pa = Corners[0].position; //Bottom-Left
-		pb = Corners[1].position; //Bottom-Right
-		pc = Corners[2].position; //Top-Left
-		pd = Corners[3].position; //Top-Right
-
-		//Debug.Log (pe);
-		
-		Vector3 vr = ( pb - pa ).normalized; // right axis of screen
-		Vector3 vu = ( pc - pa ).normalized; // up axis of screen
-		Vector3 vn = Vector3.Cross( vr, vu ).normalized; // normal vector of screen
-		
-		Vector3 va = pa - pe; // from pe to pa
-		Vector3 vb = pb - pe; // from pe to pb
-		Vector3 vc = pc - pe; // from pe to pc
-		Vector3 vd = pd - pe; // from pe to pd
-		
-		//float n = -lookTarget.InverseTransformPoint( Camera.main.transform.position ).z; // distance to the near clip plane (screen)
-		float n = near;
-		float f = Camera.main.farClipPlane; // distance of far clipping plane
-		float d = Vector3.Dot( va, vn ); // distance from eye to screen
-		float l = Vector3.Dot( vr, va ) * n / d; // distance to left screen edge from the 'center'
-		float r = Vector3.Dot( vr, vb ) * n / d; // distance to right screen edge from 'center'
-		float b = Vector3.Dot( vu, va ) * n / d; // distance to bottom screen edge from 'center'
-		float t = Vector3.Dot( vu, vc ) * n / d; // distance to top screen edge from 'center'
-		
-		Matrix4x4 p = new Matrix4x4(); // Projection matrix
-		p[0, 0] = 2.0f * n / ( r - l );
-		p[0, 2] = ( r + l ) / ( r - l );
-		p[1, 1] = 2.0f * n / ( t - b );
-		p[1, 2] = ( t + b ) / ( t - b );
-		p[2, 2] = ( f + n ) / ( n - f );
-		p[2, 3] = (2.0f * f * n / ( n - f )) * 0.5f;
-		p[3, 2] = -1.0f;
-		
-		Camera.main.projectionMatrix = p; // Assign matrix to camera
-		
-		if ( drawNearCone ) { //Draw lines from the camera to the corners f the screen
-			Debug.DrawRay( pe, va, Color.blue );
-			Debug.DrawRay( pe, vb, Color.blue );
-			Debug.DrawRay( pe, vc, Color.blue );
-			Debug.DrawRay( pe, vd, Color.blue );
-		}
-		
-		if ( drawFrustum ) DrawFrustum( Camera.main ); //Draw actual camera frustum
-		
-	}
-	
-	Vector3 ThreePlaneIntersection ( Plane p1, Plane p2, Plane p3 ) { //get the intersection point of 3 planes
-		return ( ( -p1.distance * Vector3.Cross( p2.normal, p3.normal ) ) +
-		        ( -p2.distance * Vector3.Cross( p3.normal, p1.normal ) ) +
-		        ( -p3.distance * Vector3.Cross( p1.normal, p2.normal ) ) ) /
-			( Vector3.Dot( p1.normal, Vector3.Cross( p2.normal, p3.normal ) ) );
-	}
-	
-	void DrawFrustum ( Camera cam ) {
-		Vector3[] nearCorners = new Vector3[4]; //Approx'd nearplane corners
-		Vector3[] farCorners = new Vector3[4]; //Approx'd farplane corners
-		Plane[] camPlanes = GeometryUtility.CalculateFrustumPlanes( cam ); //get planes from matrix
-		Plane temp = camPlanes[1]; camPlanes[1] = camPlanes[2]; camPlanes[2] = temp; //swap [1] and [2] so the order is better for the loop
-		
-		for ( int i = 0; i < 4; i++ ) {
-			nearCorners[i] = ThreePlaneIntersection( camPlanes[4], camPlanes[i], camPlanes[( i + 1 ) % 4] ); //near corners on the created projection matrix
-			farCorners[i] = ThreePlaneIntersection( camPlanes[5], camPlanes[i], camPlanes[( i + 1 ) % 4] ); //far corners on the created projection matrix
-		}
-		
-		for ( int i = 0; i < 4; i++ ) {
-			Debug.DrawLine( nearCorners[i], nearCorners[( i + 1 ) % 4], Color.red, Time.deltaTime, false ); //near corners on the created projection matrix
-			Debug.DrawLine( farCorners[i], farCorners[( i + 1 ) % 4], Color.red, Time.deltaTime, false ); //far corners on the created projection matrix
-			Debug.DrawLine( nearCorners[i], farCorners[i], Color.red, Time.deltaTime, false ); //sides of the created projection matrix
-		}
-	}
-
-*/
-
-
-
-
-
 
 }
--- Scripts/ObjMovementController.cs
+++ Scripts/ObjMovementController.cs
@@ -1,3 +1,6 @@
+// This script enables an object to be "grabbed" using a Leap-enabled hand gesture.
+// Associate to a "grabbableObj" by adding this script as a component to the object.
+
 using UnityEngine;
 using System.Collections;
 using Leap;
@@ -52,24 +55,21 @@ public class ObjMovementController : MonoBehaviour {
 
 		Vector3 handCoordinates = getHandCoordinates (leap_hand);
 
+		// Smooth hand coordinate signal from Leap by averaging with the most recent such coordinates
 		Vector3 meanLastFewHandPositions = getPositionsMean (lastFewHandPositions, 4);
 
 		if(lastFewHandPositions.Count > 8 && Vector3.Distance(meanLastFewHandPositions, handCoordinates) > 2.0f) {
-			// outlier
-			//return;
+			// Handle outlier
 		}
 
-		//if (leap_hand.GrabStrength == null || leap_hand.PinchStrength == null)
-		//S	return;
-
+		// Same smoothing applied to the grab/pinch strengths
 		float meanLastFewGrabStrengths = getMean (lastFewGrabStrengths, 4);
 		float meanLastFewPinchStrengthss = getMean (lastFewPinchStrengths, 4);
 
 		if(lastFewGrabStrengths.Count > 6 && 
 		   (Mathf.Abs(meanLastFewGrabStrengths - (float) leap_hand.GrabStrength) > 0.6f ||
 		   Mathf.Abs(meanLastFewPinchStrengthss - (float) leap_hand.PinchStrength) > 0.6f)) {
-			// outlier
-			//return;
+			// Handle outlier
 		}
 
 		lastFewGrabStrengths.Add ((float) leap_hand.GrabStrength);
@@ -85,8 +85,7 @@ public class ObjMovementController : MonoBehaviour {
 		}
 		Vector3 newHandPosition = getPositionsMean(lastFewHandPositions, 3);
 
-
-
+		// Ensure that the grab gesture is intentional and the behavior is expected
 		if (isGrabbing && lastFewGrabStrengths.Count > 8 && (getMean(lastFewGrabStrengths, 8) > 0.7 || getMean(lastFewPinchStrengths, 8) > 0.7)) {
 			isGrabbing = true;
 		} else if (!isGrabbing && lastFewGrabStrengths.Count > 8 && 
@@ -95,8 +94,6 @@ public class ObjMovementController : MonoBehaviour {
 		} else {
 			isGrabbing = false;
 		}
-
-
 
 		if (isGrabbing) {
 			grabbableObj.GetComponent<Animator> ().SetBool ("Grounded_b", false);
